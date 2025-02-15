6869ba5b66040699cbbf3d3ed215ecc777ab2ed0
Nicolly <133717535+SH4R4@users.noreply.github.com>
2025-02-15 09:51:49 -0300

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"************************************"<< endl;
+    cout<<"* Bem vindo ao jogo da advinhação! *"<< endl;
+    cout<<"************************************"<< endl;
+    
+    int num_sec = 22;
+   cout<<" O numero secreto é " << num_sec <<". não conte para ninguém!" << endl;
+}
