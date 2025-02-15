c8d8168ea90a5d74a22bf8573471e1844323cfe7
Nicolly <133717535+SH4R4@users.noreply.github.com>
2025-02-15 11:30:09 -0300

    Add files via upload

---
+++ pontadicionada.cpp
@@ -0,0 +1,48 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"************************************"<< endl;
+    cout<<"* Bem vindo ao jogo da advinhação! *"<< endl;
+    cout<<"************************************"<< endl;
+    
+    const int NUM_SEC = 76;
+    
+    bool nacertou = true;
+    int tentativas = 0;
+    
+    double pontos = 1000.0;
+    
+   while(nacertou){
+       tentativas++;
+       cout << "Tentativa " << tentativas << endl;
+    int chute;
+   cout << "Qual seu chute? ";
+   cin >> chute;
+   
+   double pperdidos = abs(chute - NUM_SEC)/2.0;
+   pontos = pontos - pperdidos;
+    
+   cout << "O valor do seu chute é: " << chute << endl;
+   
+   bool acertou = chute == NUM_SEC;
+   bool maior = chute > NUM_SEC;
+   
+   if (acertou){
+       cout << "Parabens, voce acertou o numero secreto!" << endl;
+       nacertou = false;
+   } else if (maior){
+       cout <<"Seu chute foi maior que o numero a ser descoberto." << endl;
+   }
+   else{
+       cout << "Seu chute foi menor que o numero a ser descoberto." << endl;
+   }
+   
+  }
+   cout << "Jogo finalizado."<< endl;
+   cout << "Você precisou de " << tentativas << " tentativas para acertar o numero secreto." << endl;
+   cout <<"vejamos..." << endl;
+   cout << pontos << " bilhões de pontos pra você!" << endl;
+}
